2e930254090ce347f1a9de349a2cbd861eb8d616
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-30 13:09:48 +0100

    Create Node.js

---
+++ balanced-bst/Node.js
@@ -0,0 +1,11 @@
+module.exports = class Node {
+  value;
+  left;
+  right;
+
+  constructor(value = null, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+};
