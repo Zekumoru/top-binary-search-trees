93c7911a8d0bf0b0683dae57631a97c94c16c779
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-30 14:09:12 +0100

    Create Tree.js

---
+++ balanced-bst/Tree.js
@@ -0,0 +1,15 @@
+const Node = require("./Node");
+
+module.exports = class Tree {
+  #root;
+
+  constructor(array) {
+    this.#root = Tree.buildTree(array);
+  }
+
+  get root() {
+    return this.#root;
+  }
+
+  static buildTree(array) {}
+};
