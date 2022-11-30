669b708d86c02372f67ef9df61acb14611bd7364
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-30 15:41:12 +0100

    Create buildTree function

--- balanced-bst/Tree.js
+++ balanced-bst/Tree.js
@@ -1,4 +1,3 @@
-const MergeSort = require('../merge-sort/MergeSort');
 const Node = require('./Node');
 
 module.exports = class Tree {
@@ -12,5 +11,17 @@ module.exports = class Tree {
     return this.#root;
   }
 
-  static buildTree(array) {}
+  static buildTree(array) {
+    return this.#buildTree(array, 0, array.length);
+  }
+
+  static #buildTree(array, start, end) {
+    if (start > end) return null;
+
+    const middle = (start + end) / 2;
+    const left = this.#buildTree(array, start, middle - 1);
+    const right = this.#buildTree(array, middle + 1, end);
+
+    return new Node(array[middle], left, right);
+  }
 };
