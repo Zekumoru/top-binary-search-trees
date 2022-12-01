b923c22194a7ad31ccf7386cc26fb92479a972be
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-01 13:43:20 +0100

    Fix prettyPrint throwing error on null root

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const isSorted = (array) => array.reduce((sorted, current, index) => {
 }, true);
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) return;
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
   }
